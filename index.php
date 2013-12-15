34defc35fa2ee771c0906edef4d4741e38cdc4b3
Ignacio Trujillo <ignacio@dawncoffee.com>
2013-12-15 19:34:52 +0100

    Minor changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <meta http-equiv="refresh" content="10; url=http://dawncoffee.com">
 <link rel="prerender" href="http://dawncoffee.com">
-<style scope>html{color:black;background:white}body,div,h3,p{margin:0;padding:0}img{border:0}h3{font-size:100%;font-weight:normal}small{font-size:75%}body{position:relative;margin:20px 0;font-family:"Palatino Linotype",Palatino,Constantia,Cambria,Times,"Times New Roman",Georgia,serif}.container{position:relative;width:960px;margin:0 auto;padding:0}@media only screen and (min-width:768px) and (max-width:959px) {.container{width:768px}}@media only screen and (max-width:767px) {.container{width:300px}}.logo{text-align:center}.logo img{width:50px;opacity:.5}h3{font-size:1.24em;margin-top:20px;font-style:italic;letter-spacing:-1px}p{font-size:1em}h3,p{text-align:center;font-weight:normal;margin-bottom:20px}footer em{font-family:Georgia,serif}</style>
+<style scope>html{color:black;background:white}body,div,h3,p{margin:0;padding:0}img{border:0}h3{font-size:100%;font-weight:normal}small{font-size:75%}body{position:relative;margin:20px 0;font-family:"Palatino Linotype",Palatino,Constantia,Cambria,Times,"Times New Roman",Georgia,serif}.container{position:relative;width:960px;margin:0 auto;padding:0}@media only screen and (min-width:768px) and (max-width:959px) {.container{width:600px}}@media only screen and (max-width:767px) {.container{width:300px}}.logo{text-align:center}.logo img{width:50px;opacity:.5}h3{font-size:1.24em;margin-top:20px;font-style:italic;letter-spacing:-1px}p{font-size:1em}h3,p{text-align:center;font-weight:normal;margin-bottom:20px}footer em{font-family:Georgia,serif}</style>
 </head>
 <body>
 
@@ -21,7 +21,7 @@
 <section>
 <div class="container">			
 <h3><span style="opacity:.3">(emails)</span> Dawncoffee</h3>
-<p>Aquí sólo nos preocupamos de la mensajería del blog. En unos segundos te llevaremos directamente al blog. Gracias por la inesperada visita.</p>
+<p>Aquí vive la mensajería del blog. En unos segundos volveras a él. Gracias por la inesperada visita.</p>
 </div>
 </section>
 
