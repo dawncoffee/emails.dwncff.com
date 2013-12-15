69bcdea58e2d1a5e9647f38ec67dc35ed933f459
Ignacio Trujillo <ignacio@dawncoffee.com>
2013-12-15 15:43:10 +0100

    Minor changes

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<!doctype html>
-<html lang="es-ES">
-<head>
-<title>(core) Dawncoffee</title>
-<meta charset="utf-8"/>
-<link rel="shortcut icon" href="http://core.images.dawncoffee.com/global/c/favicon/es/favicon.png" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<meta http-equiv="refresh" content="10; url=http://dawncoffee.com">
-<style scope>html{color:black;background:white}body,div,h3,p{margin:0;padding:0}img{border:0}h3{font-size:100%;font-weight:normal}small{font-size:75%}body{position:relative;margin:20px 0;font-family:"Palatino Linotype",Palatino,Constantia,Cambria,Times,"Times New Roman",Georgia,serif}.container{position:relative;width:960px;margin:0 auto;padding:0}@media only screen and (min-width:768px) and (max-width:959px) {.container{width:768px}}@media only screen and (max-width:767px) {.container{width:300px}}.logo{text-align:center}.logo img{width:50px;opacity:.5}h3{font-size:1.24em;margin-top:20px;font-style:italic;letter-spacing:-1px}p{font-size:1em}h3,p{text-align:center;font-weight:normal;margin-bottom:20px}footer em{font-family:Georgia,serif}</style>
-</head>
-<body>
+<?php 
 
-<header>
-<div class="container">
-<hgroup class="logo"><a href="http://dawncoffee.com"><img src="http://core.images.dawncoffee.com/all/copyright/common/footer.png" alt="Dawncoffee"/></a></hgroup>
-</div>
-</header>
-
-<section>
-<div class="container">			
-<h3><span style="opacity:.3">(emails)</span> Dawncoffee</h3>
-<p><a href="http://emails.dwncff.com/courrier/es/aniversario/20131118/">Aniversario 2013</a></p>
-</div>
-</section>
-
-<footer>
-<div class="container">	
-<p><small>&copy; <script>document.write(new Date().getFullYear())</script> <em>Dawncoffee</em></small></p>
-</div>
-</footer>
-
-</body>
-</html>
+header('Location: http://at.dawncoffee.com');?>
